7fe38314785ed97c89d6724d950878bd5e7000cb
arge <omerrsenem@gmail.com>
2023-06-07 19:44:28 +0300

    bismillah

---
+++ lib/views/home_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+      body:  SafeArea(
+        child:
+        Column(
+
+          children: [
+
+            Expanded(
+
+              child: WebView(
+                  allowsInlineMediaPlayback: true,
+                  debuggingEnabled: false,
+                  initialMediaPlaybackPolicy:
+                  AutoMediaPlaybackPolicy.always_allow,
+
+                initialUrl: "http://localhost:3001",
+                javascriptMode: JavascriptMode.unrestricted,
+                javascriptChannels: {
+                  JavascriptChannel(
+                    name: "flutter",
+                    onMessageReceived: (JavascriptMessage message) {
+                      if (message.message == "onSubscribeClick") {
+                        print("onSubscribeClick");
+                      }
+                    },
+                  )
+                },
+
+              ),
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
